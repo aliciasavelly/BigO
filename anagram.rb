49bf1db2c90e46b47a6dabef0a5b205b2dc605ba
aliciasavelly <alicia.savelly@gmail.com>
2017-02-05 00:27:05 -0800

    clean up anagram code

--- anagram.rb
+++ anagram.rb
@@ -2,26 +2,32 @@ def anagram?(str1, str2)
   str1.chars.permutation.map(&:join).include?(str2)
 end
 
-puts anagram?("gizmo", "sally")    #=> false
-puts anagram?("elvis", "lives")    #=> true
+puts anagram?("gizmo", "sally") == false
+puts anagram?("elvis", "lives") == true
 
-# def anagram2?(str1, str2)
-#   temp1 = str1
-#   temp2 = str2
-#   str1.chars.each do |char|
-#     str1.chars.each do |char
-#     end
-#   end
-# end
+def anagram2?(str1, str2)
+  arr1, arr2 = str1.chars, str2.chars
+
+  arr1.each do |char|
+    target_idx = arr2.find_index(char)
+    return false unless target_idx
+    arr2.delete_at(target_idx)
+  end
+
+  arr2.empty?
+end
+
+puts anagram2?("gizmo", "sally") == false
+puts anagram2?("elvis", "lives") == true
+
 
 def anagram3?(str1, str2)
   str1.sort == str2.sort && str1 != str2
 end
 
 def two_sum(nums, target)
-  nums = nums.sort
+  nums = nums.dup.sort
   i = 0
-  while
 end
 
 def two_sum(nums, target)
