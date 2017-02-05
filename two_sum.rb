1efea14084dae828878785daa167306ef7474bfe
aliciasavelly <alicia.savelly@gmail.com>
2017-02-05 00:37:53 -0800

    move two sum to its own file

---
+++ two_sum.rb
@@ -0,0 +1,11 @@
+def two_sum(nums, target)
+  nums = nums.dup.sort
+  i = 0
+end
+
+def two_sum(nums, target)
+  # iterature thru arr of nums, then create a hash using num as key,
+  # values as indices
+  # then you would check to see if c bomination fot he keys  is == target,
+  # if u do throw in all valid indices pairs
+end
